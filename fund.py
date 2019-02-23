cbf6bf008c7b7382638e7b33325288beb2e72694
blahblah <amanda720.rose@gmail.com>
2019-02-23 15:11:28 -0700

    1st commit

---
+++ fund.py
@@ -0,0 +1,21 @@
+import pickle as p
+import pandas as pd
+
+phil = "oppenheimerWebData.pkl"
+
+df = pd.read_pickle(phil)
+dimDF = len(df)
+
+# column values
+values = df.columns.values	
+dimVal = len(values)
+
+# extract data into list
+visitorID = df['visitor-ids'].tolist()
+userID = df['user-ids'].tolist()
+time = df['timestamp'].tolist()
+web = df['webpage'].tolist()
+state = df['state'].tolist()
+browser = df['browser'].tolist()
+platform = df['platform'].tolist()
+
